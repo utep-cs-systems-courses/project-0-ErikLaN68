d6ef933b5b128b4347f9a3ee98e5d5ce18854680
ErikLaN68 <eriklaneave@gmail.com>
2023-06-20 14:44:50 -0600

    Add comments to better explain what is happening

--- draw_chars.c
+++ draw_chars.c
@@ -5,8 +5,10 @@ void print_char_11x16(char c)
 {
   c -= 0x20;
   for (char row = 0; row < 11; row++) {
+    //This short could be moved into the for loop below depending on how the font is put together
     unsigned short rowBits = font_11x16[c][row];
     for (char col = 0; col < 16; col++) {
+      //makes a binary string of 1s and compares them with rowbits to see when both are 1s
       unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
@@ -17,9 +19,11 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
   c -= 0x20;
+  //used 10 instead of 8 because of dead space in font
   for (char row = 0; row < 10; row++) {
     unsigned short rowBits = font_8x12[c][row];
     for (char col = 0; col < 12; col++) {
+      //This is shifting the bits to the left by adding zeros to the right
       unsigned short colMask = 1 << (11-col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
